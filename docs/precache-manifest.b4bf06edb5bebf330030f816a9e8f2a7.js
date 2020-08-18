455968a9347f22b18da544f0d3fcd21036bdfc85
Ignacio Iglesias <iiglesiascampoy@gmail.com>
2020-08-18 17:30:00 -0600

    docs added

---
+++ docs/precache-manifest.b4bf06edb5bebf330030f816a9e8f2a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d631a1350c0de6c7b97b33f0439fa32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78bc5d6bfe4a5cef435e",
+    "url": "/static/css/main.5b760f16.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78bc5d6bfe4a5cef435e",
+    "url": "/static/js/main.958e1974.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
